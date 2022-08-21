8db5af93f29e50dedb5b93b05d5dd8a72a84971f
Bente <nittka@cl.uni-heidelberg.de>
2022-08-21 10:19:13 +0200

    add script for dataset statistics

---
+++ dataset_statistics.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+NUM_FILES = 16
+
+data = pd.read_csv(f'data/data_part_{1}_extended.csv', sep=',', header=0)
+data['created_at'] = pd.to_datetime(data['created_at'])
+
+aggressiveness_counts =  data['aggressiveness'].value_counts()
+stance_counts =  data['stance'].value_counts()
+topic_counts =  data['topic'].value_counts()
+gender_counts =  data['gender'].value_counts()
+
+for i in range(1,NUM_FILES):
+
+    data = pd.read_csv(f'data/data_part_{i+1}_extended.csv', sep=',', header=0)
+    data['created_at'] = pd.to_datetime(data['created_at'])
+
+    aggressiveness_counts +=  data['aggressiveness'].value_counts()
+    stance_counts +=  data['stance'].value_counts()
+    topic_counts +=  data['topic'].value_counts()
+    gender_counts +=  data['gender'].value_counts()
+
+
+print("####### Sampled Data ###########")
+print(aggressiveness_counts)
+print(stance_counts)
+print(topic_counts)
+print(gender_counts)
+
+print("###### Statement data ##############")
+data = pd.read_csv(f'data/statement_data.csv', sep=',', header=0)
+data['created_at'] = pd.to_datetime(data['created_at'])
+
+aggressiveness_counts =  data['aggressiveness'].value_counts()
+stance_counts =  data['stance'].value_counts()
+topic_counts =  data['topic'].value_counts()
+gender_counts =  data['gender'].value_counts()
+
+print(aggressiveness_counts)
+print(stance_counts)
+print(topic_counts)
+print(gender_counts)
+
+aggressiveness_counts =  data['aggressiveness'].value_counts(normalize=True)
+stance_counts =  data['stance'].value_counts(normalize=True)
+topic_counts =  data['topic'].value_counts(normalize=True)
+gender_counts =  data['gender'].value_counts(normalize=True)
+
+print(aggressiveness_counts)
+print(stance_counts)
+print(topic_counts)
+print(gender_counts)
