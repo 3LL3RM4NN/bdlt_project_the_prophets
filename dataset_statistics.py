ab6400fa6173c2e3e26443355e2a3e3b74d012f6
bnittka <44901801+bnittka@users.noreply.github.com>
2022-08-26 19:19:46 +0200

    Update dataset_statistics.py

--- dataset_statistics.py
+++ dataset_statistics.py
@@ -1,13 +1,14 @@
 '''
 Script to extract statistics about the different datasets in order to compare them
 
-Statistics include value counts for
+Statistics include value counts (and in part percentages) for
     - aggressiveness
     - stance
     - gender
     - topic 
 
 for each of the following dataset variants
+    - original data (not included in this repo)
     - sampled original data
     - statement data
     - deduplicated (and classified) statements
@@ -23,29 +24,31 @@ import seaborn as sns
 
 NUM_FILES = 16
 
-data = pd.read_csv(f'data/data_part_{1}.csv', sep=',', header=0)
-data['created_at'] = pd.to_datetime(data['created_at'])
+### commented out because original data is not included in this repo ###
 
-aggressiveness_counts =  data['aggressiveness'].value_counts()
-stance_counts =  data['stance'].value_counts()
-topic_counts =  data['topic'].value_counts()
-gender_counts =  data['gender'].value_counts()
+# data = pd.read_csv(f'data/data_part_{1}.csv', sep=',', header=0)
+# data['created_at'] = pd.to_datetime(data['created_at'])
 
-for i in range(1,NUM_FILES):
+# aggressiveness_counts =  data['aggressiveness'].value_counts()
+# stance_counts =  data['stance'].value_counts()
+# topic_counts =  data['topic'].value_counts()
+# gender_counts =  data['gender'].value_counts()
 
-    data = pd.read_csv(f'data/data_part_{i+1}.csv', sep=',', header=0)
-    data['created_at'] = pd.to_datetime(data['created_at'])
+# for i in range(1,NUM_FILES):
 
-    aggressiveness_counts +=  data['aggressiveness'].value_counts()
-    stance_counts +=  data['stance'].value_counts()
-    topic_counts +=  data['topic'].value_counts()
-    gender_counts +=  data['gender'].value_counts()
+#     data = pd.read_csv(f'data/data_part_{i+1}.csv', sep=',', header=0)
+#     data['created_at'] = pd.to_datetime(data['created_at'])
 
-print("####### Sampled Data ###########")
-print(aggressiveness_counts)
-print(stance_counts)
-print(topic_counts)
-print(gender_counts)
+#     aggressiveness_counts +=  data['aggressiveness'].value_counts()
+#     stance_counts +=  data['stance'].value_counts()
+#     topic_counts +=  data['topic'].value_counts()
+#     gender_counts +=  data['gender'].value_counts()
+
+# print("####### Original Data ###########")
+# print(aggressiveness_counts)
+# print(stance_counts)
+# print(topic_counts)
+# print(gender_counts)
 
 
 # --------------------------------------------------------------------------------------------------
